c1846821da1b10245d388983b5c0794eb83da2f1
Caio Gonçalves <caio.sfg@gmail.com>
2019-10-22 23:54:13 -0300

    Inserido o Tabuleiro

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -8,21 +8,28 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Campo campo = new Campo(8, 8);
 
-                campo.colocarPeca(new Torre(campo, Cor.Preta), new Posicao(0, 0));
-                campo.colocarPeca(new Torre(campo, Cor.Preta), new Posicao(1, 3));
-                campo.colocarPeca(new Rei(campo, Cor.Preta), new Posicao(2, 4));
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
+            //try
+            //{
+
+
+            //    Campo campo = new Campo(8, 8);
+
+            //    campo.colocarPeca(new Torre(campo, Cor.Preta), new Posicao(0, 0));
+            //    campo.colocarPeca(new Torre(campo, Cor.Preta), new Posicao(1, 3));
+            //    campo.colocarPeca(new Rei(campo, Cor.Preta), new Posicao(2, 4));
                 
 
-                Tela.imprimirTabuleiro(campo);
-            }
-            catch(TabuleiroExceptions e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            //    Tela.imprimirTabuleiro(campo);
+            //}
+            //catch(TabuleiroExceptions e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
             Console.ReadLine();
         }
     }
